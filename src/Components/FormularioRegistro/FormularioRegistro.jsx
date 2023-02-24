ccb33c9b67b348e569cd33dc6a95d485c617af9b
Hernan <hschiavonr@gmail.com>
2023-02-24 10:32:53 +0100

    first up

---
+++ src/Components/FormularioRegistro/FormularioRegistro.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+
+const FormularioRegistro = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+  
+    const register = () => {
+      axios.post('/api/register', { email: email, password: password })
+        .then((response) => {
+          setMessage(response.data);
+          setEmail('');
+          setPassword('');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  
+    const login = () => {
+      axios.post('/api/login', { email: email, password: password })
+        .then((response) => {
+          localStorage.setItem('token', response.data.token);
+          setMessage('Inicio de sesión exitoso');
+          setEmail('');
+          setPassword('');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  
+    const logout = () => {
+      localStorage.removeItem('token');
+      setMessage('Sesión cerrada');
+    };
+  
+    return (
+      <div className="App">
+        <h1>Formulario de Registro</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="button" onClick={register}>
+            Registrarse
+          </button>
+          <button type="button" onClick={login}>
+            Iniciar sesión
+          </button>
+          <button type="button" onClick={logout}>
+            Cerrar sesión
+          </button>
+        </form>
+        <p>{message}</p>
+      </div>
+    );
+}
+
+export default FormularioRegistro
